027480cb42b321005e37e7fe9344f45f0c12e994
Eli Almario <ee.almario@gmail.com>
2021-03-28 20:43:18 +0800

    Svelte practice

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	import ContactCard from './ContactCard.svelte';
+
+	let name = '';
+	let job = '';
+	let desc = '';
+	let imgUrl = '';
+</script>
+
+<style>
+	.form {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+</style>
+
+<div class="form">
+	<label for="imgUrl">Image URL:</label>
+	<input type="text" bind:value={imgUrl} id="imgUrl"/>
+	<label for="name">Name:</label>
+	<input type="text" bind:value={name} id="name"/>
+	<label for="name">Job:</label>
+	<input type="text" bind:value={job} id="job"/>
+	<label for="desc">Short Description:</label>
+	<textarea bind:value={desc} id="desc"/>
+</div>
+
+<ContactCard
+	{imgUrl}
+	{name}
+	{job}
+	{desc}
+/>
---
+++ src/ContactCard.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let name;
+  export let job;
+  export let desc;
+  export let imgUrl;
+</script>
+
+<style>
+  .container {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  .contact-card {
+    border-radius:  8px;
+    box-shadow: 0 8px 24px  rgba(0,0,0, 0.15);
+    margin: 0 auto;
+    padding: 1rem 2rem;
+    width: 300px;
+    height: 400px;
+  }
+
+  .cc-header img {
+    width: 100%;
+    height: auto;
+  }
+
+  .divider {
+    background: gray;
+    margin: 1.5rem 0;
+    opacity: 0.2;
+    height: 1px;
+    width: 100%;
+  }
+</style>
+
+<div class="container">
+  <div class="contact-card">
+    <div class="cc-header">
+      <img src={imgUrl} alt="avatar">
+      <div class="cc-details">
+        <h4>{name}</h4>
+        <p>{job}</p>
+      </div>
+    </div>
+    <div class="divider"></div>
+    <div class="cc-body">
+      {desc}
+    </div>
+  </div>
+</div>
